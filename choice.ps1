bacbeb26b54bbc2025f066ba3fbde11a74061bb7
Markus Gruber <GruberMarkus@users.noreply.github.com>
2020-06-24 12:11:35 +0200

    add option -allowTypeAhead, update description

--- choice.ps1
+++ choice.ps1
@@ -1,91 +1,94 @@
 Function Choice {
-    <#
-    .DESCRIPTION
-    Asks the user to choose from a configurable list of options.
-    Loops until a valid option has been chosen or the timout is reached.
-    Supports a default option to be chosen in case of timeout of user presses the Enter key.
+  <#
+  .DESCRIPTION
+  Asks the user to choose from a configurable list of options.
+  Loops until a valid option has been chosen or the timout is reached.
+  Supports a default option to be chosen in case of timeout of user presses the Enter key.
+  Can also be used as a replace for the "pause" command ("Press any key to continue...")
+  .PARAMETER message
+  Specifies the message to be shown.
+  Allowed choices are automatically generated out of every single letter written between square brackets.
+  For example: "Continue? [Y]es or [N]o" makes Y and N acceptable choices (not case sensitive).
+  The first letter in square brackets preceeded by > is set as default value.
+  For example: "Continue? [Y]es or >[N]o" makes N the default choice when the timeout is reached or the user presses Enter.
+  When message contains no choices, every keypress is accepted and Enter as default choice.
+  The message can be a multi-line string:
+  choice "Continue?`n[Y] or >[N]o" -timeOutSeconds -1
+  .PARAMETER timeOutSeconds
+  The number of seconds until the timeout is reached.
+  0 means that the default choice is applied immediately.
+  -1 or any other value less than 0 disables the timeout.
+  .PARAMETER allowTypeAhead
+  When set to $false, the keyboard input buffer is cleared right before the message is displayed.
+  This is useful in scenarios where a script pops up while the user is working on something else and might just continue typing.
+  When set to $false, script automation via input redirection to a file ("<"-sign in common shells) stops at this point.
+  .INPUTS
+  The message can be piped:
+  "Continue?`n[Y] or >[N]o" | choice -timeOutSeconds -1
+  .OUTPUTS
+  String containing pressed valid key or default value, else $null.
+  .EXAMPLE
+  choice "Continue?`n[Y] or [N]o" -timeOutSeconds -1
+  "Continue?`n[Y] or >[N]o" | choice -timeOutSeconds -5
+  #>
 
-    .PARAMETER message
-    Specifies the message to be shown.
+  param (
+    [parameter(ValueFromPipeline)]
+    [string]$message = $null,
+    [int]$timeOutSeconds = -1,
+    [bool]$allowTypeAhead = $true
+  )
 
-    Allowed choices are automatically generated out of every single letter written between square brackets.
-    For example: "Continue? [Y]es or [N]o" makes Y and N acceptable choices (not case sensitive).
+  $regexPattern = ($message | Select-String -Pattern '(?<=\[).?(?=\])' -AllMatches).matches.value -join ''
+  If ($regexPattern) {
+    $regexPattern = "[$regexPattern]"
+  } else {
+    $regexPattern = '.?'
+  }
 
-    The first letter in square brackets preceeded by > is set as default value.
-    For example: "Continue? [Y]es or >[N]o" makes N the default choice when the timeout is reached or the user presses Enter.
+  $defaultValue = ($message | Select-String '(?<=\>\[).?(?=\])').matches.value -join ''
+  $key = $null
 
-    When message contains no choices, every keypress is accepted and Enter as default choice.
+  if (![string]::IsNullOrEmpty($message)) {
+    Write-Host -NoNewline $message
+  }
 
-    The message can be a multi-line string:
-    choice "Continue?`n[Y] or >[N]o" -timeOutSeconds -1
+  $counter = $timeOutSeconds * 10
 
-    .PARAMETER timeOutSeconds
-    The number of seconds until the timeout is reached.
-    0 means that the default choice is applied immediately.
-    -1 or any other value less than 0 disables the timeout.
-
-    .INPUTS
-    The message can be piped:
-    "Continue?`n[Y] or >[N]o" | choice -timeOutSeconds -1
-
-    .OUTPUTS
-    String containing pressed valid key or default value, else $null.
-
-    .EXAMPLE
-    choice "Continue?`n[Y] or [N]o" -timeOutSeconds -1
-    "Continue?`n[Y] or >[N]o" | choice -timeOutSeconds -5
-    #>
-
-    param (
-        [parameter(ValueFromPipeline)]
-        [string]$message = $null,
-        [int]$timeOutSeconds = -1
-    )
-
-    $regexPattern = ($message | Select-String -Pattern '(?<=\[).?(?=\])' -AllMatches).matches.value -join ''
-    If ($regexPattern) {
-        $regexPattern = "[$regexPattern]"
+  if (-not $allowTypeAhead) {
+    while ($Host.UI.RawUI.KeyAvailable) {
+      $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,IncludeKeyUp")
+      $key = $null
     }
-    else {
-        $regexPattern = '.?'
+  }
+
+  while ($key -eq $null -and ($timeOutSeconds -lt 0 -or ($counter-- -gt 0))) {
+    if (($timeOutSeconds -eq 0) -or $Host.UI.RawUI.KeyAvailable) {
+      $key_ = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,IncludeKeyUp")
+      if ($key_.KeyDown -and (($key_.Character -match $regexPattern) -or ($key_.VirtualKeyCode -eq 13))) {
+        $key = $key_.Character
+      }
+    } else {
+      Start-Sleep -m 100 # milliseconds
     }
+  }
 
-    $defaultValue = ($message | Select-String '(?<=\>\[).?(?=\])').matches.value -join ''
-    $key = $null
-    $Host.UI.RawUI.FlushInputBuffer()
-
-    if (![string]::IsNullOrEmpty($message)) {
-        Write-Host -NoNewline $message
+  if (-not ($key -eq $null)) {
+    if ($key_.VirtualKeyCode -eq 13) {
+      $key = $defaultValue
+      if ($defaultValue -eq '') { Write-Host }
+    } else {
+      Write-Host -NoNewline "$($key.Character)"
     }
+  } else {
+    $key = $defaultValue
+    if ($defaultValue -eq '') { Write-Host }
+  }
 
-    $counter = $timeOutSeconds * 10
-
-
-    while ($key -eq $null -and ($timeOutSeconds -lt 0 -or ($counter-- -gt 0))) {
-        if (($timeOutSeconds -eq 0) -or $Host.UI.RawUI.KeyAvailable) {
-            $key_ = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,IncludeKeyUp")
-            if ($key_.KeyDown -and (($key_.Character -match $regexPattern) -or ($key_.VirtualKeyCode -eq 13))) {
-                $key = $key_.Character
-            }
-        }
-        else {
-            Start-Sleep -m 100 # milliseconds
-        }
-    }
-
-    if (-not ($key -eq $null)) {
-        if ($key_.VirtualKeyCode -eq 13) {
-            $key = $defaultValue
-            if ($defaultValue -eq '') { Write-Host }
-        }
-        else {
-            Write-Host -NoNewline "$($key.Character)"
-        }
-    }
-    else {
-        $key = $defaultValue
-        if ($defaultValue -eq '') { Write-Host }
-    }
-
-    return $(if (($key -eq $null) -or ($key -eq "")) { $null } else { $key })
+  if (($key -eq $null) -or ($key -eq "")) {
+    return $null
+  } else {
+    Write-Host $key
+    return $key
+  }
 }
